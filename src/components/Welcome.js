486fee750064d91b9ca28c12cfa256482820366d
George Grammenidis <grammenidis@yahoo.gr>
2020-10-20 10:41:37 +0300

    added padding to header of the table, removed useWrapper from Welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,6 @@ import React from 'react'
 import useWrapper from '../hooks/useWrapper';
 
 export default function Welcome() {
-    useWrapper("users")
     return (
         <>
             <h2>Welcome</h2>
